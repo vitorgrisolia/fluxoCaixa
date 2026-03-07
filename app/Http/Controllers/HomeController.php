82560d67f25951a2a01bd04f458d188a6894a29f
Vitor Grisolia <grisolia.vitor@gmail.com>
2026-03-07 17:43:21 -0300

    modified:   app/Http/Controllers/HomeController.php 	modified:   resources/views/home/index.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Lancamento;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $user = Auth::user();
+
+        $baseQuery = Lancamento::query()
+            ->where('id_user', $user->id_user);
+
+        $totalLancamentos = (clone $baseQuery)->count();
+
+        $totalEntradas = (clone $baseQuery)
+            ->whereHas('centroCusto.tipo', function ($query) {
+                $query->where(function ($subQuery) {
+                    $subQuery->whereRaw('LOWER(tipo) LIKE ?', ['%entrada%'])
+                        ->orWhereRaw('LOWER(tipo) LIKE ?', ['%receita%']);
+                });
+            })
+            ->sum('valor');
+
+        $totalSaidas = (clone $baseQuery)
+            ->whereHas('centroCusto.tipo', function ($query) {
+                $query->where(function ($subQuery) {
+                    $subQuery->whereRaw('LOWER(tipo) LIKE ?', ['%saida%'])
+                        ->orWhereRaw('LOWER(tipo) LIKE ?', ['%despesa%']);
+                });
+            })
+            ->sum('valor');
+
+        $ultimosLancamentos = (clone $baseQuery)
+            ->with(['centroCusto.tipo'])
+            ->orderBy('dt_faturamento', 'desc')
+            ->limit(8)
+            ->get();
+
+        return view('home.index')->with([
+            'totalLancamentos' => $totalLancamentos,
+            'totalEntradas' => $totalEntradas,
+            'totalSaidas' => $totalSaidas,
+            'saldoAtual' => $totalEntradas - $totalSaidas,
+            'ultimosLancamentos' => $ultimosLancamentos,
+        ]);
     }
 
     public function create()
