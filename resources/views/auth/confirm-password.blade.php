00c31f88ded7ca3216808f8615a1874c16b2ddd2
Vitor Grisolia <grisolia.vitor@gmail.com>
2026-03-13 18:36:21 -0300

    traducao para portugues brasil

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -7,7 +7,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+            {{ __('Esta é uma área segura do aplicativo. Por favor, confirme sua senha antes de continuar..') }}
         </div>
 
         <!-- Validation Errors -->
@@ -28,7 +28,7 @@
 
             <div class="flex justify-end mt-4">
                 <x-primary-button>
-                    {{ __('Confirm') }}
+                    {{ __('Confirmar') }}
                 </x-primary-button>
             </div>
         </form>
